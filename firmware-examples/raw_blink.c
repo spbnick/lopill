5d5e2b80e4b0ce21c0d85e35789819a5cbc7d9c7
Nikolai Kondrashov <spbnick@gmail.com>
2020-10-03 08:57:28 +0300

    Switch to using the new rcc.h

--- firmware-examples/raw_blink.c
+++ firmware-examples/raw_blink.c
@@ -1,14 +1,15 @@
+#include <rcc.h>
+
 int
 main(void)
 {
-    volatile unsigned int *rcc_iopenr = (unsigned int *)(0x40021000 + 0x2c);
     volatile unsigned int *gpioc_moder = (unsigned int *)(0x50000800 + 0x00);
     volatile unsigned int *gpioc_otyper = (unsigned int *)(0x50000800 + 0x04);
     volatile unsigned int *gpioc_odr = (unsigned int *)(0x50000800 + 0x14);
     volatile unsigned int i;
 
     /* Enable clock to GPIO port C */
-    *rcc_iopenr |= 1 << 2;
+    RCC->iopenr |= 1 << 2;
 
     /* Configure the PC13 pin for open-drain output, 2MHz max speed */
     *gpioc_moder = (*gpioc_moder & ~ (0x3 << (13 * 2))) |
