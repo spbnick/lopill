d3c04a775883e39fc05b6c94bb01f06fe2920102
Nikolai Kondrashov <spbnick@gmail.com>
2020-10-03 16:34:23 +0300

    Use the new gpio.h

--- firmware-examples/raw_blink.c
+++ firmware-examples/raw_blink.c
@@ -1,26 +1,24 @@
 #include <rcc.h>
+#include <gpio.h>
 
 int
 main(void)
 {
-    volatile unsigned int *gpioc_moder = (unsigned int *)(0x50000800 + 0x00);
-    volatile unsigned int *gpioc_otyper = (unsigned int *)(0x50000800 + 0x04);
-    volatile unsigned int *gpioc_odr = (unsigned int *)(0x50000800 + 0x14);
     volatile unsigned int i;
 
     /* Enable clock to GPIO port C */
     RCC->iopenr |= 1 << 2;
 
     /* Configure the PC13 pin for open-drain output, 2MHz max speed */
-    *gpioc_moder = (*gpioc_moder & ~ (0x3 << (13 * 2))) |
+    GPIO_C->moder = (GPIO_C->moder & ~ (0x3 << (13 * 2))) |
                    (1 << (13 * 2));
-    *gpioc_otyper |= 1 << 13;
+    GPIO_C->otyper |= 1 << 13;
 
     /* Forever */
     while (1) {
         /* Wait */
         for (i = 0; i < 100000; i++);
         /* Toggle PC13 */
-        *gpioc_odr ^= 1 << 13;
+        GPIO_C->odr ^= 1 << 13;
     }
 }
