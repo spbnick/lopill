b0da988cee5b41f9c94326e7148ff1d1ae1fd61b
Nikolai Kondrashov <spbnick@gmail.com>
2020-10-03 17:15:48 +0300

    Use constants everywhere

--- firmware-examples/raw_blink.c
+++ firmware-examples/raw_blink.c
@@ -7,18 +7,18 @@ main(void)
     volatile unsigned int i;
 
     /* Enable clock to GPIO port C */
-    RCC->iopenr |= 1 << 2;
+    RCC->iopenr |= RCC_IOPENR_IOPCEN_MASK;
 
-    /* Configure the PC13 pin for open-drain output, 2MHz max speed */
-    GPIO_C->moder = (GPIO_C->moder & ~ (0x3 << (13 * 2))) |
-                   (1 << (13 * 2));
-    GPIO_C->otyper |= 1 << 13;
+    /* Configure the PC13 pin for open-drain output */
+    GPIO_C->otyper |= (GPIO_OTYPE_OPEN_DRAIN << GPIO_OTYPER_OT13_LSB);
+    GPIO_C->moder = (GPIO_C->moder & ~GPIO_MODER_MODE13_MASK) |
+                    (GPIO_MODE_OUTPUT<< GPIO_MODER_MODE13_LSB);
 
     /* Forever */
     while (1) {
         /* Wait */
         for (i = 0; i < 100000; i++);
         /* Toggle PC13 */
-        GPIO_C->odr ^= 1 << 13;
+        GPIO_C->odr ^= GPIO_ODR_OD13_MASK;
     }
 }
