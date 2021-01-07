9df4d05ad88ad4bdd4924140633557011d0d7211
Nikolai Kondrashov <spbnick@gmail.com>
2021-01-07 14:03:44 +0200

    Switch blink.c to GPIO config functions

--- firmware-examples/blink.c
+++ firmware-examples/blink.c
@@ -14,12 +14,8 @@ main(void)
     RCC->iopenr |= RCC_IOPENR_IOPCEN_MASK;
 
     /* Configure the PC13 pin for open-drain output */
-    GPIO_C->otyper = (GPIO_C->otyper & ~GPIO_OTYPER_OT13_MASK) |
-                     (GPIO_OTYPE_OPEN_DRAIN << GPIO_OTYPER_OT13_LSB);
-    GPIO_C->moder = (GPIO_C->moder & ~GPIO_MODER_MODE13_MASK) |
-                    (GPIO_MODE_OUTPUT << GPIO_MODER_MODE13_LSB);
-    GPIO_C->ospeedr = (GPIO_C->ospeedr & ~GPIO_OSPEEDR_OSPEED13_MASK) |
-                      (GPIO_OSPEED_HIGH << GPIO_OSPEEDR_OSPEED13_LSB);
+    gpio_pin_conf_output(GPIO_C, 13, GPIO_OTYPE_OPEN_DRAIN,
+                         GPIO_PUPD_NONE, GPIO_OSPEED_HIGH);
 
     /* Forever */
     while (1) {
